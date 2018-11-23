a6c28a8ba36e9d466bfd8b33b92bdf542b1036ef
Kristian Zarebski <kristian.alexander.zarebski@cern.ch>
2018-11-23 16:24:44 +0100

    modulised

--- pyturtle/__init__.py
+++ pyturtle/__init__.py
@@ -7,26 +7,31 @@ class Turtle(object):
     def __init__(self):
         self._direction   = np.array([0, pi/2.])  # Theta-Phi Angles in Radians
         self._current_pos = np.array([0,0,0]) # Start at Origin
-        self._log = [[self._current_pos]]
-        self._pendown = True
+        self._pendown = False
+        self._log = [[]]
 
     def _deg2rad(self, angle):
         return angle*pi/180.
 
+    def _log_point(self, vector):
+            vector = list(vector)
+            if self._pendown:
+                for i,j  in enumerate(vector):
+                    if abs(vector[i]) < 1E-2:
+                        vector[i] = int(vector[i])
+                self._log[-1].append(vector)
+
     def _get_vector_from_angle(self):
         theta, phi = self._direction
-        print(np.array([sin(phi)*cos(theta), sin(phi)*sin(theta), cos(phi)]))
         return np.array([sin(phi)*cos(theta), sin(phi)*sin(theta), cos(phi)])
 
     def forward(self, dist):
-        self._current_pos = self._current_pos + dist*self._get_vector_from_angle()
-        if self._pendown:
-            self._log[-1].append(self._current_pos)
+        self._current_pos = self._current_pos - dist*self._get_vector_from_angle()
+        self._log_point(self._current_pos)
 
     def backward(self, dist):
         self._current_pos = self._current_pos + dist*self._get_vector_from_angle()
-        if self._pendown:
-            self._log[-1].append(self._current_pos)
+        self._log_point(self._current_pos)
 
     def right(self, angle):
         self._direction[0] += self._deg2rad(angle)
@@ -46,21 +51,25 @@ class Turtle(object):
 
     def pendown(self):
         self._pendown = True
-        self._log[-1].append(self._current_pos)
+        self._log_point(self._current_pos)
+
+    def _check_if_2d(self):
+        for route in self._log:
+            for point in route:
+                if point[2] > 1E-3:
+                    return True
+        return False
 
     def plot(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+        if self._check_if_2d():
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+        plt.clf()
         for route in self._log:
-            print(route)
             route = np.array(route)
-            ax.plot(route[:,0], route[:,1], route[:,2])
+            if self._check_if_2d():
+                ax.plot(route[:,0], route[:,1], route[:,2])
+            else:
+                plt.plot(route[:,0], route[:,1], route[:,2])
         plt.show()
 
-if __name__ in "__main__":
-    x = Turtle()
-    x.elevate(10)
-    for i in range(100):
-        x.forward(5)
-        x.right(5)
-    x.plot()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup( name             =       'PyTurtle3D'                                     ,
+       version          =       'v0.1.0'                                         ,
+       description      =       'MatplotLib based Turtle Class'                  ,
+       url              =       'https://github.com/artemis-beta/pyturtle3d'     ,
+       author           =       'Kristian Zarebski'                              ,
+       author_email     =       'krizar312@yahoo.co.uk'                          ,
+       license          =       'MIT'                                            ,
+       packages         =       ['pyturtle']                                     ,
+       zip_safe         =       False                                            ,
+       install_requires =       ['numpy', 'matplotlib']
+     )
