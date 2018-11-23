fd1edd7b5a1fc24e489db42a48df9f3b5175ddbe
Kristian Zarebski <kristian.alexander.zarebski@cern.ch>
2018-11-23 15:13:48 +0100

    Started 3D devlopment

--- pyturtle/__init__.py
+++ pyturtle/__init__.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from math import pi, sin, cos
+from mpl_toolkits.mplot3d import Axes3D
 
 class Turtle(object):
     def __init__(self):
-        self._direction   = 0               # Angle in Radians
-        self._current_pos = np.array([0,0]) # Start at Origin
+        self._direction   = np.array([0, pi/2.])  # Theta-Phi Angles in Radians
+        self._current_pos = np.array([0,0,0]) # Start at Origin
         self._log = [[self._current_pos]]
         self._pendown = True
 
@@ -13,8 +14,9 @@ class Turtle(object):
         return angle*pi/180.
 
     def _get_vector_from_angle(self):
-        angle = self._direction
-        return np.array([sin(angle), cos(angle)])
+        theta, phi = self._direction
+        print(np.array([sin(phi)*cos(theta), sin(phi)*sin(theta), cos(phi)]))
+        return np.array([sin(phi)*cos(theta), sin(phi)*sin(theta), cos(phi)])
 
     def forward(self, dist):
         self._current_pos = self._current_pos + dist*self._get_vector_from_angle()
@@ -27,10 +29,16 @@ class Turtle(object):
             self._log[-1].append(self._current_pos)
 
     def right(self, angle):
-        self._direction += self._deg2rad(angle)
+        self._direction[0] += self._deg2rad(angle)
 
     def left(self, angle):
-        self._direction -= self._deg2rad(angle)
+        self._direction[0] -= self._deg2rad(angle)
+
+    def elevate(self, angle):
+        self._direction[1] += self._deg2rad(angle)
+
+    def declinate(self, angle):
+        self._direction[1] -= self._deg2rad(angle)
 
     def penup(self):
         self._pendown = False
@@ -41,22 +49,18 @@ class Turtle(object):
         self._log[-1].append(self._current_pos)
 
     def plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
         for route in self._log:
+            print(route)
             route = np.array(route)
-            plt.plot(route[:,0], route[:,1])
+            ax.plot(route[:,0], route[:,1], route[:,2])
         plt.show()
 
 if __name__ in "__main__":
     x = Turtle()
-    x.forward(10)
-    x.right(90)
-    x.forward(10)
-    x.right(45)
-    x.forward(10)
-    x.penup()
-    x.right(45)
-    x.forward(10)
-    x.left(90)
-    x.pendown()
-    x.forward(10)
+    x.elevate(10)
+    for i in range(100):
+        x.forward(5)
+        x.right(5)
     x.plot()
